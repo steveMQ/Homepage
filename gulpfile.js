5ce4776a9a4bb653ad51feda7569ba67ac2397aa
steveMQ <stevequatrale@gmail.com>
2015-09-19 19:05:06 -0400

    Setup stuff complete!

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,13 @@
 var gulp = require('gulp');
+var gulpless = require('gulp-less');
 
+
+gulp.task('less', function(){
+  gulp.src('less/main.less')
+  .pipe(gulpless())
+  .pipe(gulp.dest('less'));
+});
+
+gulp.task('default', ['less'], function(){
+  gulp.watch('less/*.less', ['less']);
+});
--- header.php
+++ header.php
@@ -4,14 +4,19 @@
 <?php wp_head(); ?>
 <link href="<?php echo get_stylesheet_uri(); ?>" type="text/css" rel="stylesheet" />
 
+
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<!-- importing my main.css file -->
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/less/main.css" />
+
 </head>
 
 <body>
