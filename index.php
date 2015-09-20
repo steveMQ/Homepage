8299aeb4a08b86fca6197738e8ead7fea681e49c
steveMQ <stevequatrale@gmail.com>
2015-09-20 19:44:01 -0400

    added in the mouse scroll

--- index.php
+++ index.php
@@ -5,12 +5,16 @@
       <div class="container">
         <div class="row">
           <div class="subheader-holder">
-            <h1>hi, i'm steve</h1>
-            <p>wordpress developer</br>from boston</p>
+            <h1>{ hi. i'm steve };</h1>
+            <p>// wordpress developer</br>// from boston</p>
           </div>
         </div>
       </div>
     </div>
+    
+    <!--white mouse at the bottom of the subheader area -->
+    <img src="<?php echo get_template_directory_uri(); ?>/imgs/mouse-scroll.png" class="mouse-scroll" />
+
   </section>
 
 
