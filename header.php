d5eeaebb692a38f63e4df0d27b1b158cbf940288
steveMQ <stevequatrale@gmail.com>
2015-09-20 19:04:41 -0400

    color tweaks for header

--- header.php
+++ header.php
@@ -43,8 +43,8 @@
             <li><a href="">Home</a></li>
             <li><a href="">Work</a></li>
             <li><a href="">Skills</a></li>
-            <li><a href="">Home</a></li>
-            <li><a href="">Home</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contact</a></li>
           </ul>
         </nav>
 
