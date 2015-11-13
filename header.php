bfc74d455d6c49bdc7bff09e1bbe6822abd879f4
steveMQ <stevequatrale@gmail.com>
2015-11-13 14:44:24 -0500

    small stuff

--- header.php
+++ header.php
@@ -43,7 +43,6 @@
             <li><a href="">Home</a></li>
             <li><a href="">Work</a></li>
             <li><a href="">Skills</a></li>
-            <li><a href="">Portfolio</a></li>
             <li><a href="">Contact</a></li>
           </ul>
         </nav>
